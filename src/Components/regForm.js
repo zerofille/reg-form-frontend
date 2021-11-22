eb9a6f5880e2cd7b0af3e17f10dda213a1bd8d9d
zerofille <nstrdnv@gmai.com>
2021-11-22 12:35:19 +0200

    Create form

---
+++ src/Components/regForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+function RegForm(props) {
+  const [passwordState, setPasswordState] = useState(false);
+  const changePasswordState = () => {
+    setPasswordState(!passwordState);
+  };
+  return (
+    <section>
+      <Formik>
+        {(formikProps) => {
+          return (
+            <Form>
+              <h1>Make an account</h1>
+              <Field type="text" name="email" placeholder="email" />
+              <div>
+                <Field
+                  name="password"
+                  placeholder="password"
+                  type={passwordState ? "text" : "password"}
+                />
+                <Field
+                  name="passwordConf"
+                  placeholder="confirm password"
+                  type={passwordState ? "text" : "password"}
+                />
+              </div>
+              <div>
+                <Field
+                  type="checkbox"
+                  onChange={changePasswordState}
+                  checked={passwordState}
+                  name="isShownPassword"
+                />
+                <p>Show password</p>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </section>
+  );
+}
+
+export default RegForm;
