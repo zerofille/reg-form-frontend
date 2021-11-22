eb0bbc9b9088abe0e87c742d9aa3477ac13f3c96
zerofille <nstrdnv@gmai.com>
2021-11-22 13:44:24 +0200

    Add small fix

--- src/actions/userActionCreators.js
+++ src/actions/userActionCreators.js
@@ -1,17 +1,17 @@
-import ACTION_TYPES from "./actionTypes"
+import ACTION_TYPES from "./actionTypes";
 
 export const createUserRequest = (values) =>{
     return {
         type:ACTION_TYPES.CREATE_USER_REQUEST,
         payload:values
     }
-},
+};
 export const createUserSuccess = (values) =>{
     return {
         type:ACTION_TYPES.CREATE_USER_SUCCESS,
         payload:values
     }
-},
+};
 export const createUserError = (error) =>{
     return {
         type:ACTION_TYPES.CREATE_USER_ERROR,
